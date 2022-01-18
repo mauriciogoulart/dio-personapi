ae6bb97df44dd2e272dde5ae4b6fafc95825e512
mauriciogoulart <mauryciorgoulart@yahoo.com.br>
2022-01-18 20:21:03 -0300

    first commit

---
+++ src/main/java/br/com/mg/personapi/DioPersonapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mg.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPersonapiApplication.class, args);
+	}
+
+}
