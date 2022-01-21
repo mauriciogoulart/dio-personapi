4a500bc82921ea27eddd6d7d6ad5a697298bc1c0
mauriciogoulart <mauryciorgoulart@yahoo.com.br>
2022-01-20 22:24:26 -0200

    Added delete and put operations on PersonController.

--- src/main/java/br/com/mg/personapi/controller/PersonController.java
+++ src/main/java/br/com/mg/personapi/controller/PersonController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mg.personapi.dto.PersonDTO;
@@ -31,18 +34,32 @@ public class PersonController {
 		PersonDTO savedPerson = service.create(personDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
 	}
-	
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping(path = "/{identificationDocument}")
+	public void delete(@PathVariable("identificationDocument") String identificationDocument)
+			throws PersonNotFoundException {
+		service.delete(identificationDocument);
+	}
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<PersonDTO>> findAll() {
 		List<PersonDTO> people = service.findAll();
-		return ResponseEntity.ok(people);	
+		return ResponseEntity.ok(people);
 	}
-	
-	@GetMapping(path = "/{identificationDocument}",produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(path = "/{identificationDocument}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PersonDTO> findById(@PathVariable("identificationDocument") String identificationDocument)
 			throws PersonNotFoundException {
 		PersonDTO person = service.findById(identificationDocument);
-		return ResponseEntity.ok(person);	
+		return ResponseEntity.ok(person);
+	}
+
+	@PutMapping(path = "/{identificationDocument}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<PersonDTO> update(@PathVariable("identificationDocument") String identificationDocument,
+			@RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+		PersonDTO person = service.update(identificationDocument, personDTO);
+		return ResponseEntity.ok(person);
 	}
 
 }
--- src/main/java/br/com/mg/personapi/service/PersonService.java
+++ src/main/java/br/com/mg/personapi/service/PersonService.java
@@ -3,6 +3,9 @@ package br.com.mg.personapi.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,11 @@ public class PersonService {
 		return personMapper.toDTO(person);
 	}
 
+	public void delete(String identificationDocument) throws PersonNotFoundException {
+		Person person = findPersonById(identificationDocument);
+		repository.delete(person);
+	}
+
 	public List<PersonDTO> findAll() {
 		List<Person> people = repository.findAll();
 		List<PersonDTO> peopleDTO = people.stream().map(personMapper::toDTO).collect(Collectors.toList());
@@ -33,9 +41,21 @@ public class PersonService {
 	}
 
 	public PersonDTO findById(String identificationDocument) throws PersonNotFoundException {
-		Person person = repository.findById(identificationDocument)
-				.orElseThrow(() -> new PersonNotFoundException(identificationDocument));
+		Person person = findPersonById(identificationDocument);
 		return personMapper.toDTO(person);
+	}
+
+	private Person findPersonById(String identificationDocument) throws PersonNotFoundException {
+		return repository.findById(identificationDocument)
+				.orElseThrow(() -> new PersonNotFoundException(identificationDocument));
+	}
+
+	public PersonDTO update(String identificationDocument, @Valid PersonDTO personDTO) throws PersonNotFoundException {
+		Person persistedPerson = findPersonById(identificationDocument);
+		Person personToSave = personMapper.toModel(personDTO);
+		BeanUtils.copyProperties(personToSave, persistedPerson);
+		Person updatedPerson = repository.save(persistedPerson);
+		return personMapper.toDTO(updatedPerson);
 	}
 
 }
